f9cf3464929097ba23e87131cd9ecf5706f25417
ashqures <ashqures@cisco.com>
2017-04-06 22:16:53 +0530

    Count all path in 2d matrix with obstruction

--- src/com/concept/java/solution/NoOfPaths.java
+++ src/com/concept/java/solution/NoOfPaths.java
@@ -14,7 +14,9 @@ package com.concept.java.solution;
 * http://algorithms.tutorialhorizon.com/dynamic-programming-count-all-paths-from-top-left-to-bottom-right-of-a-mxn-matrix/
 *
 *
+*  Dynamic Programming — Count all paths in 2D Matrix with Obstructions in it
 *
+*  http://algorithms.tutorialhorizon.com/dynamic-programming-count-all-paths-in-2d-matrix-with-obstructions-in-it/
 *
 * */
 public class NoOfPaths extends Base{
@@ -44,10 +46,46 @@ public class NoOfPaths extends Base{
         return result[result.length-1][result.length-1];
     }
     
+    public int countDPWithObstruction(int matrix[][]){
+        int result[][] = new int[matrix.length][matrix.length];
+        result[0][0] = 1;
+        for (int i = 0; i < result.length; i++) {
+            if (matrix[0][i] != -1)
+                result[0][i] = 1;
+            else
+                result[0][i] = 1;
+        }
+        for (int j = 0; j < result.length; j++) {
+            if (matrix[j][0] != -1)
+                result[j][0] = 1;
+            else
+                result[j][0] = 1;
+        }
+        for(int i=1;i<result.length;i++){
+            for(int j=1;j<result.length;j++){
+                if(matrix[i][j]!=-1){
+                    result[i][j]=0;
+                    if(result[i-1][j]>0){
+                        result[i][j] = result[i][j]+result[i-1][j];
+                    }
+                    if(result[i][j-1]>0){
+                        result[i][j] = result[i][j]+result[i][j-1];
+                    }
+                }
+            }
+        }
+        printArray(result);
+        return result[matrix.length-1][matrix.length-1];
+    }
+    
     public static void main(String args[]){
         int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
         NoOfPaths noOfPaths = new NoOfPaths();
         System.out.println("Recursive result:- "+noOfPaths.count(matrix,0,0));
         System.out.println("DP result:- "+noOfPaths.countDP(matrix));
+
+        System.out.println("Path with obstruction");
+        int arrA [][] = {{1,1,1},{1,-1,1},{1,-1,1}};
+        System.out.println("Path with obstruction DP result:- "+noOfPaths.countDPWithObstruction(arrA));
     }
 }
