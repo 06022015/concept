a4c98cbfd11d714baa8e810ab7a50939421dc37d
ashqures <ashqures@cisco.com>
2017-04-20 08:51:35 +0530

    determine if a binary tree is height-balanced

---
+++ src/com/concept/java/solution/IsBinaryTreeBalanced.java
@@ -0,0 +1,49 @@
+package com.concept.java.solution;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ashqures
+ * Date: 4/20/17
+ * Time: 8:24 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+* How to determine if a binary tree is height-balanced?
+* */
+public class IsBinaryTreeBalanced extends Base{
+
+
+    /*
+   * Time Complexity O(n)
+   * Space Complexity O(n)
+   * */
+    private int checkBalanced(Node root){
+        if(null == root)
+            return 0;
+        int leftSubTree = checkBalanced(root.left);
+        if(leftSubTree == -1)
+            return -1;
+        int rightSubTree = checkBalanced(root.right);
+        if(rightSubTree == -1)
+            return -1;
+        if(Math.abs(leftSubTree - rightSubTree)>1)
+            return -1;
+        return Math.max(leftSubTree,rightSubTree)+1;
+    }
+
+    /*
+    * Time Complexity O(n)
+    * Space Complexity O(n)
+    * */
+    public boolean isBalanced(Node root){
+         if(null == root)
+             return true;
+        return !(checkBalanced(root)==-1);
+    }
+    
+    public static void main(String args[]){
+        IsBinaryTreeBalanced isBalanced = new IsBinaryTreeBalanced();
+        Node root = isBalanced.getBinaryTree();
+        System.out.println("Balanced:- "+ isBalanced.isBalanced(root));
+    }
+}
