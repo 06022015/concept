71f253b34e3f00dcc1286b83906ed64084dad333
ashqures <ashqures@cisco.com>
2017-07-08 23:24:45 +0530

    Brute force methode added for finding longest string which is palindrome

--- src/com/concept/java/arrays/LongestPalindrome.java
+++ src/com/concept/java/arrays/LongestPalindrome.java
@@ -15,7 +15,6 @@ import com.concept.java.Base;
 * */
 public class LongestPalindrome extends Base {
 
-
     /*
     * O(n2) Time complexity
     * */
@@ -44,9 +43,39 @@ public class LongestPalindrome extends Base {
         }
         return input.substring(start, end);
     }
+
+
+    /*
+   * O(n2) Time complexity
+   * */
+    public static String longestPalindromeBruteForce(String input){
+        if(null == input || "".equals(input))
+            return input;
+        int index = 0;
+        String result = "";
+        while(index < input.length()){
+            int low = index;
+            int high = index;
+            while(high < input.length()-1 && input.charAt(low) == input.charAt(high+1)){
+                high = high+1;
+            }
+            String text = input.substring(low, high+1);
+            low = low-1;
+            high = high+1;
+            while(low>=0 && high< input.length() && input.charAt(low) == input.charAt(high)){
+                text = input.substring(low, high+1);
+                low = low-1;
+                high= high+1;
+            }
+            if(result.length() < text.length()){
+                result = text;
+            }
+            index++;
+        }
+        return result;
+    }
     
     public static void main(String args[]){
-        LongestPalindrome longestPalindrome = new LongestPalindrome();
         System.out.println(longestPalindrome("forgeeksskeegfor"));
         System.out.println(longestPalindrome("a"));
         System.out.println(longestPalindrome("aa"));
@@ -57,5 +86,18 @@ public class LongestPalindrome extends Base {
         System.out.println(longestPalindrome("aabaaaabaa"));
         System.out.println(longestPalindrome("aabafabaa"));
         System.out.println(longestPalindrome("aabafabaaq"));
+
+        System.out.println("\n\n\n\n\n");
+
+        System.out.println(longestPalindromeBruteForce("forgeeksskeegfor"));
+        System.out.println(longestPalindromeBruteForce("a"));
+        System.out.println(longestPalindromeBruteForce("aa"));
+        System.out.println(longestPalindromeBruteForce("aaaa"));
+        System.out.println(longestPalindromeBruteForce("aab"));
+        System.out.println(longestPalindromeBruteForce("aaba"));
+        System.out.println(longestPalindromeBruteForce("aabaa"));
+        System.out.println(longestPalindromeBruteForce("aabaaaabaa"));
+        System.out.println(longestPalindromeBruteForce("aabafabaa"));
+        System.out.println(longestPalindromeBruteForce("aabafabaaq"));
     }
 }
